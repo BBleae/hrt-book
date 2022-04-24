060a1e18dab16159c1e86ea712575919f99180a1
Ayaka <26200808+KiritaniAyaka@users.noreply.github.com>
2022-04-24 23:22:43 +0800

    Initial commit

---
+++ docs/.vuepress/dist/assets/index.html.62badb33.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"Hello VuePress",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{},filePathRelative:"README.md"};export{e as data};
---
+++ vuepress.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+	lang: 'zh-CN',
+	title: 'MtF HRT Docs',
+	description: 'MtF HRT Docs',
+	head: [
+		['link', { rel: 'icon', href: './flag.png' }]
+	],
+	theme: '@vuepress/theme-default',
+	themeConfig: {
+		logo: './flag.png',
+	},
+}
