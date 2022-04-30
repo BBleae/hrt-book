ac5aa0e709e362bf311bfd57d24a6d33970319ba
Ayaka <26200808+KiritaniAyaka@users.noreply.github.com>
2022-04-30 16:32:42 +0800

    build: add markdown-it plugins

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,46 +1,52 @@
-import type { ThemeConfig } from '@vuepress/core'
+import { defineUserConfig } from 'vuepress'
+import markdownItAttrs from 'markdown-it-attrs'
+import markdownItBracketedSpans from 'markdown-it-bracketed-spans'
 
-const config: ThemeConfig = {
-  lang: "zh-CN",
-  title: "Shizu's HRT Guide",
-  description: "Shizu's HRT Guide for transfemale",
-  head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }],
-    ["link", { rel: "stylesheet", href: "/custom.css", type: "text/css" }],
-  ],
-  theme: "@vuepress/theme-default",
-  themeConfig: {
-    logo: "/logo.png",
-    repo: "BBleae/hrt-book",
-    repoLabel: "GitHub",
-    docsRepo: "BBleae/hrt-book",
-    docsDir: "docs",
-    docsBranch: "main",
-    editLinks: true,
-    editLinkText: "编辑本页面",
-    contributors: true,
+const config = defineUserConfig({
+	lang: "zh-CN",
+	title: "Shizu's HRT Guide",
+	description: "Shizu's HRT Guide for transfemale",
+	head: [
+		["link", { rel: "icon", href: "/favicon.ico" }],
+		["link", { rel: "stylesheet", href: "/custom.css", type: "text/css" }],
+	],
+	theme: "@vuepress/theme-default",
+	themeConfig: {
+		logo: "/logo.png",
+		repo: "BBleae/hrt-book",
+		repoLabel: "GitHub",
+		docsRepo: "BBleae/hrt-book",
+		docsDir: "docs",
+		docsBranch: "main",
+		editLinks: true,
+		editLinkText: "编辑本页面",
+		contributors: true,
 		contributorsText: "贡献者",
-    lastUpdated: false,
-    externalLinkIcon: true,
-    navbar: [
-      {
-        text: "首页",
-        link: "/",
-      },
-      {
-        text: "目录",
-        link: "/menu/",
-      },
-      {
-        text: "链接",
-        link: "/link/",
-      },
-      {
-        text: "Credits",
-        link: "/credits/",
-      },
-    ],
-  },
-};
+		lastUpdated: false,
+		externalLinkIcon: true,
+		navbar: [
+			{
+				text: "首页",
+				link: "/",
+			},
+			{
+				text: "目录",
+				link: "/menu/",
+			},
+			{
+				text: "链接",
+				link: "/link/",
+			},
+			{
+				text: "Credits",
+				link: "/credits/",
+			},
+		],
+	},
+	extendsMarkdown: (md, app) => {
+		md.use(markdownItBracketedSpans)
+		md.use(markdownItAttrs)
+	}
+})
 
 module.exports = config
